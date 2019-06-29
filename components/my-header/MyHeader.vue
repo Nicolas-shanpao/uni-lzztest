52aed814d18296380295857bf07a3bdb057b3d3e
liangzz <375621354@qq.com>
2019-06-29 15:41:40 +0800

    模板

---
+++ components/my-header/MyHeader.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="cu-bar bg-main">
+		<view class="action" @tap="BackPage">
+			<text class="cuIcon-back text-white"></text> 返回
+		</view>
+		<view class="content text-bold">
+			操作条
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {};
+		},
+		name: 'my-header',
+		computed: {},
+		props: {},
+		methods: {
+			BackPage() {
+				uni.navigateBack({
+					delta: 1,
+					animationType: 'pop-out',
+					animationDuration: 200
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ layout/default.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="defaultpage">
+		<MyHeader />
+		<view class="content" :style="[{top:StatusBar + 'px'}]">
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	import MyHeader from '@/components/my-header/MyHeader.vue'
+
+	export default {
+		components: {
+			MyHeader
+		}
+	}
+</script>
+<style scoped>
+	.defaultpage {
+		padding-top: 70upx;
+	}
+
+	.content {
+		height: calc(100vh - 170upx);
+		overflow-y: scroll;
+	}
+</style>
